a483fae37922769a3d9033e03b16d5a49aee2818
Daniil Gerachshenko <danikger@gmail.com>
2024-11-29 14:01:00 -0600

    Updates link to the github repo in the footer

--- video-compressor/src/App.jsx
+++ video-compressor/src/App.jsx
@@ -314,7 +314,7 @@ function App() {
         {/* FOOTER */}
         <footer className="bg-zinc-900 absolute bottom-0 left-0 right-0">
           <div className="w-full mx-auto max-w-screen-md p-4 flex items-center justify-center">
-            <span className="text-sm text-zinc-400 text-center">2024 VidPress | <a href="https://www.ffmpeg.org/" className="hover:underline">GitHub</a> | Powered by <a href="https://www.ffmpeg.org/" className="hover:underline">FFmpeg</a>.</span>
+            <span className="text-sm text-zinc-400 text-center">2024 VidPress | <a href="https://github.com/danikger/Video-Compressor" className="hover:underline">GitHub</a> | Powered by <a href="https://www.ffmpeg.org/" className="hover:underline">FFmpeg</a>.</span>
           </div>
         </footer>
       </main >
