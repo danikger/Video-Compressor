8b66588dff7dc8da5bb137b5f368f07c3d2bdd5c
Daniil Gerachshenko <danikger@gmail.com>
2024-12-02 13:47:22 -0600

    Adds react router to main.jsx
    Routing setup for when/if i want to add more features than just video compression

--- video-compressor/src/main.jsx
+++ video-compressor/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.jsx'
+import App from "./App.jsx";
+import NotFound from "./Pages/notFound.jsx";
+import ImageCompression from "./Pages/imageCompression.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
