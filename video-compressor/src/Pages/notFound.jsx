ce32ea22db8d8ed5c4d93469f77e5683b88c1fbd
Daniil Gerachshenko <danikger@gmail.com>
2024-12-02 13:43:47 -0600

    Adds a 'not found' page

---
+++ video-compressor/src/Pages/notFound.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router";
+
+export default function NotFound() {
+  return (
+    <>
+      <main className="bg-zinc-900 min-h-screen relative grid place-items-center">
+        <div className="text-center">
+          <h1 className="text-5xl font-semibold text-zinc-200 mb-6">Page not found</h1>
+          <NavLink to="/" className="items-center py-2.5 px-3.5 bg-green-500 rounded-md text-zinc-900 text-sm font-medium hover:bg-green-600">Go Back</NavLink>
+        </div>
+      </main >
+    </>
+  );
+}
