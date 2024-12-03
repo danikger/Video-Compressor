85a2ac4e59c9c8e18039bc59941ced2ef7a3c6ce
Daniil Gerachshenko <danikger@gmail.com>
2024-12-02 19:10:25 -0600

    Changes text scaling in 'notFound' page

--- video-compressor/src/Pages/notFound.jsx
+++ video-compressor/src/Pages/notFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <>
       <main className="bg-zinc-900 min-h-screen relative grid place-items-center">
         <div className="text-center">
-          <h1 className="text-5xl font-semibold text-zinc-200 mb-6">Page not found</h1>
+          <h1 className="text-4xl sm:text-5xl font-semibold text-zinc-200 mb-6">Page not found</h1>
           <NavLink to="/" className="items-center py-2.5 px-3.5 bg-green-500 rounded-md text-zinc-900 text-sm font-medium hover:bg-green-600">Go Back</NavLink>
         </div>
       </main >
