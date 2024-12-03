70e4b886c97213617a5ad0eaed5a0e23fe195d28
Daniil Gerachshenko <danikger@gmail.com>
2024-12-03 16:51:43 -0600

    Removes all references to "Vidpress"
    Prepares it for splitting into a separate repo that i can slightly tweak and host as "vidpress" and keep this as "video compressor"

--- video-compressor/src/Components/Common/footer.jsx
+++ video-compressor/src/Components/Common/footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="bg-zinc-900 absolute bottom-0 left-0 right-0">
       <div className="w-full mx-auto max-w-screen-md p-4 flex items-center justify-center">
-        <span className="text-sm text-zinc-400 text-center">2024 VidPress | <a target="_blank" href="https://github.com/danikger/Video-Compressor" className="hover:underline">GitHub</a> | Powered by <a target="_blank" href="https://www.ffmpeg.org/" className="hover:underline">FFmpeg</a>.</span>
+        <span className="text-sm text-zinc-400 text-center">2024 Daniil | <a target="_blank" href="https://github.com/danikger/Video-Compressor" className="hover:underline">GitHub</a> | Powered by <a target="_blank" href="https://www.ffmpeg.org/" className="hover:underline">FFmpeg</a>.</span>
       </div>
     </footer>
   );
--- video-compressor/src/Components/Common/header.jsx
+++ video-compressor/src/Components/Common/header.jsx
@@ -3,8 +3,8 @@ function Header() {
     <nav className="bg-zinc-800 border-zinc-700">
       <div className="max-w-screen-md flex flex-wrap items-center justify-start mx-auto py-4 px-4 md:px-0">
         <a href="/" className="flex items-center space-x-3">
-          <img draggable="false" src="/logo.svg" className="h-8" alt="VidPress Logo" />
-          <span className="text-2xl font-semibold whitespace-nowrap text-zinc-200">VidPress</span>
+          <img draggable="false" src="/logo.svg" className="h-8" alt="Logo" />
+          <span className="text-2xl font-semibold whitespace-nowrap text-zinc-200">Video Compressor</span>
         </a>
       </div>
     </nav>
